2baa599b548a161ed16672bf72e369cc11a3015d
robert-office <robertuillians123@gmail.com>
2022-02-12 21:55:20 -0300

    fix: responsividade do menu lateral 100% responsivo para todos os dispositivos mobiles

--- frontend/src/components/menuItens/MenuItens.tsx
+++ frontend/src/components/menuItens/MenuItens.tsx
@@ -17,69 +17,121 @@ export const MenuItens = ({ text, icon, href, sub }: IheaderMenuItens) => {
 
     if (sub) {
         return (
+
             <>
-                <button onClick={() => {
-                    setDropdown(!dropdown);
-                    setDropdownIcon(!dropdownIcon);
-                }}
-                    className={"w-full flex flex-row h-14 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-between px-8"}>
-                    <div style={{ width: LGmatches ? "24%" : "100%" }} className="h-full flex">
-                        {icon}
-                    </div>
+                {LGmatches ? (
+                    <>
+                        <button onClick={() => {
+                            setDropdown(!dropdown);
+                            setDropdownIcon(!dropdownIcon);
+                        }}
+                            className={"w-full flex flex-row h-14 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-between px-8"}>
+                            <div style={{ width: "24%"  }} className="h-full flex">
+                                {icon}
+                            </div>
 
-                    <div style={{ width: "70%" }} className="h-full hidden lg:flex">
-                        <p className="text-fontColor text-left font-sans font-medium text-base self-center">
-                            {text}
-                        </p>
-                    </div>
+                            <div style={{ width: "70%" }} className="h-full flex">
+                                <p className="text-fontColor text-left font-sans font-medium text-base self-center">
+                                    {text}
+                                </p>
+                            </div>
 
-                    <div style={{ width: "6%" }} className="h-full hidden lg:flex">
-                        <p className="text-fontColor text-left font-sans font-medium text-base self-center">
-                            <ArrowDropDownIcon className={ dropdownIcon ? "rotate-180 transition-transform self-center" : "transition-transform self-center"} sx={{ color: "#C9D1D9" }} />
-                        </p>
-                    </div>
-                </button>
-                <SubItenDropdown className={dropdown ? "max-h-96 flex flex-col" : "max-h-0"}>
+                            <div style={{ width: "6%" }} className="h-full flex">
+                                <p className="text-fontColor text-left font-sans font-medium text-base self-center">
+                                    <ArrowDropDownIcon className={dropdownIcon ? "rotate-180 transition-transform self-center" : "transition-transform self-center"} sx={{ color: "#C9D1D9" }} />
+                                </p>
+                            </div>
+                        </button>
+                        <SubItenDropdown className={dropdown ? "max-h-96 flex flex-col" : "max-h-0"}>
 
-                    {sub.map((subItem, id) => {
-                        return (
-                            <NavLink key={`${id}_subMenu`} to={subItem.href} className={"w-full flex flex-row h-14 border-collapse bg-containerColor hover:bg-primaryColorHovered justify-between px-8"}>
+                            {sub.map((subItem, id) => {
+                                return (
+                                    <NavLink key={`${id}_subMenu`} to={subItem.href} className={"w-full flex flex-row h-14 border-collapse bg-containerColor hover:bg-primaryColorHovered justify-between px-8"}>
 
-                                <div style={{ width: LGmatches ? "24%" : "100%" }} className="h-full flex">
-                                    {!LGmatches ? subItem.icon : ""}
-                                </div>
+                                        <div style={{ width: "24%" }} className="h-full flex">
+                                            
+                                        </div>
 
-                                <div style={{ width: "70%" }} className="h-full hidden lg:flex">
-                                    <p className="text-fontColor text-left font-sans font-medium text-base self-center">
-                                        {subItem.text}
-                                    </p>
-                                </div>
+                                        <div style={{ width: "70%" }} className="h-full flex">
+                                            <p className="text-fontColor text-left font-sans font-medium text-base self-center">
+                                                {subItem.text}
+                                            </p>
+                                        </div>
 
-                                <div style={{ width: "6%" }} className="h-full hidden lg:flex">
+                                        <div style={{ width: "6%" }} className="h-full flex">
 
-                                </div>
-                            </NavLink>
-                        )
-                    })}
-                </SubItenDropdown>
+                                        </div>
+                                    </NavLink>
+                                )
+                            })}
+                        </SubItenDropdown>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={() => {
+                            setDropdown(!dropdown);
+                            setDropdownIcon(!dropdownIcon);
+                        }}
+                            className={"w-full flex flex-col space-y-3 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-center px-4 py-2"}>
+                            <div style={{ width: "100%" }} className="flex justify-center">
+                                {icon}
+                            </div>
+
+                            <div style={{ width: "100%" }}>
+                                <p className="text-fontColor text-center font-sans font-medium text-[8px] self-center">
+                                    {text}
+                                </p>
+                            </div>
+
+                            <div style={{ width: "100%" }}>
+                                <p className="text-fontColor text-center font-sans font-medium text-[8px] self-center">
+                                    <ArrowDropDownIcon className={dropdownIcon ? "rotate-180 transition-transform self-center" : "transition-transform self-center"} sx={{ color: "#C9D1D9" }} />
+                                </p>
+                            </div>
+                        </button>
+                        <SubItenDropdown className={dropdown ? "max-h-96 flex flex-col" : "max-h-0"}>
+
+                            {sub.map((subItem, id) => {
+                                return (
+                                    <NavLink key={`${id}_subMenu`} to={subItem.href} className={"w-full flex flex-col border-collapse bg-containerColor hover:bg-primaryColorHovered justify-center space-y-3 px-4 py-2"}>
+
+                                        <div style={{ width: "100%" }} className="flex justify-center">
+                                            {subItem.icon}
+                                        </div>
+
+                                        <div style={{ width: "100%" }}>
+                                            <p className="text-fontColor text-center font-sans font-medium text-[8px] self-center">
+                                                {subItem.text}
+                                            </p>
+                                        </div>
+
+                                        <div style={{ width: "100%" }}>
+
+                                        </div>
+                                    </NavLink>
+                                )
+                            })}
+                        </SubItenDropdown>
+                    </>
+                )}
             </>
         )
     }
     else {
         return (
-            <NavLink to={href} className={(nav) => nav.isActive ? "w-full flex flex-row h-14 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-between px-8" : "w-full flex flex-row h-14 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-between px-8"}>
+            <NavLink to={href} className={"w-full flex flex-col lg:flex-row lg:h-14 border-collapse border-y border-borderColor bg-primaryColor hover:bg-primaryColorHovered justify-between lg:px-8 px-4 lg:py-0  py-2 space-y-3"}>
 
-                <div style={{ width: LGmatches ? "24%" : "100%" }} className="h-full flex">
+                <div style={{ width: LGmatches ? "24%" : "100%" }} className="flex lg:justify-start justify-center">
                     {icon}
                 </div>
 
-                <div style={{ width: "70%" }} className="h-full hidden lg:flex">
-                    <p className="text-fontColor text-left font-sans font-medium text-base self-center">
+                <div style={{ width: LGmatches ? "70%" : "100%" }}>
+                    <p className="text-fontColor lg:text-left text-center font-sans font-medium lg:text-base text-[8px] self-center">
                         {text}
                     </p>
                 </div>
 
-                <div style={{ width: "6%" }} className="h-full hidden lg:flex">
+                <div style={{ width: LGmatches ? "6%" : "100%" }}>
 
                 </div>
             </NavLink>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
   ],
   theme: {
     extend: {
+      
+
       colors: {
         ...colors,
         primaryColor: "#090910",
