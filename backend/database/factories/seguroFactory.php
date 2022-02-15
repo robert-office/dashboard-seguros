7b567f8a3cb1584806b8868daf72d5e7f04c1e7f
robert-office <robertuillians123@gmail.com>
2022-02-15 16:15:47 -0300

    add: factories para popular o db

---
+++ backend/database/factories/seguroFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\seguro>
+ */
+class seguroFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
