883356b0627dc740c5aab2526e3e5e151f1c8abd
robert-office <robertuillians123@gmail.com>
2022-02-21 17:33:31 -0300

    fixs: correções básicas para os btns da paginação e tamanho principal do container

--- frontend/src/components/paginationLink/PaginationLink.tsx
+++ frontend/src/components/paginationLink/PaginationLink.tsx
@@ -12,16 +12,16 @@ export default function PaginationLink({ content, totalPages }: Props) {
     const query = new URLSearchParams(useLocation().search);
     const page = parseInt(query.get(`${content}Page`) || '1', 10);
 
+
     return (
         <Pagination
             page={page}
             count={parseInt(totalPages)}
             size={'small'}
             color="primary"
-            hidePrevButton hideNextButton
             renderItem={(item) => (
                 <PaginationItem
-                    sx={{color: 'white'}}
+                    sx={{color: '#FF2D20'}}
                     component={Link}
                     to={`?${content}Page=${item.page}`}
                     {...item}
