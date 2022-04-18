09b3090ef55becb83f8a4df72102849f42138c89
robert-office <robertuillians123@gmail.com>
2022-04-18 17:30:18 -0300

    seeder problem

--- backend/database/factories/UserRoleFactory.php
+++ backend/database/factories/UserRoleFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserRole>
  */
-class UserRoleFactory extends Factory
+class userRoleFactory extends Factory
 {
     protected $model = userRole::class;
 
