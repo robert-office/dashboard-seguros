aa57d6aded4c7f509e98485b392458e3bed6a2cf
robert-office <robertuillians123@gmail.com>
2022-04-18 19:40:34 -0300

    erro no cors

--- frontend/src/utils/LaravelUtils/http.ts
+++ frontend/src/utils/LaravelUtils/http.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+/// alow origin
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const httpService = axios.create({
     baseURL: `https://acompany-seguros.herokuapp.com/api`
 });
