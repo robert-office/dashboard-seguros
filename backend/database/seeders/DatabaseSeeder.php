ea630fbce73600fc2236d931d90eafb5b8c7f1a5
robert-office <robertuillians123@gmail.com>
2022-04-18 15:55:21 -0300

    readme do projeto editado

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -20,47 +20,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        /// cria 300 clientes
+        cliente::factory()->count(300)->create()
+            ->each(
+                function ($cliente) {
+                    /// para cada cliente cria-se um veiculo
+                    $veiculo = veiculo::factory()->create([
+                        'id_cliente' => $cliente->id
+                    ]);
 
-        // cliente::factory()->count(300)->create()
-        //     ->each(
-        //         function ($cliente) {
-        //             /// para cada cliente cria-se um veiculo
-        //             $veiculo = veiculo::factory()->create([
-        //                 'id_cliente' => $cliente->id
-        //             ]);
+                    /// cria um user do sistema
+                    $user = User::factory()->create();
+                    /// adiciona a sua role no db
+                    $role = userRole::factory()->create([
+                        'id_user' => $user->id
+                    ]);
 
-        //             /// cria um user do sistema
-        //             $user = User::factory()->create();
-        //             /// adiciona a sua role no db
-        //             $role = userRole::factory()->create([
-        //                 'id_user' => $user->id
-        //             ]);
+                    /// se é vendedor
+                    if ($role->id_role == 2) {
 
-        //             /// se é vendedor
-        //             if ($role->id_role == 2) {
+                        /// cria o rastreador
+                        $rastreador = rastreador::factory()->create();
 
-        //                 /// cria o rastreador
-        //                 $rastreador = rastreador::factory()->create();
-
-        //                 /// cria o seguro
-        //                 $seguro = seguro::factory()->create([
-        //                     'id_vendedor'       => $user->id,
-        //                     'id_rastreador'     => $rastreador->id,
-        //                     'id_veiculo'        => $veiculo->id,
-        //                     'id_cliente'        => $cliente->id,
-        //                 ]);
-        //             }
-        //         }
-        //     );
+                        /// cria o seguro
+                        seguro::factory()->create([
+                            'id_vendedor'       => $user->id,
+                            'id_rastreador'     => $rastreador->id,
+                            'id_veiculo'        => $veiculo->id,
+                            'id_cliente'        => $cliente->id,
+                        ]);
+                    }
+                }
+            );
 
 
-        /// cria um user do sistema com lore automatica
-        // User::factory()->count(50)->create()->each(function($user) {
-        //     /// adiciona a sua role no db
-        //     userRole::factory()->create([
-        //         'id_user' => $user->id
-        //     ]);
-        // });
+        // cria um user do sistema com lore automatica
+        User::factory()->count(50)->create()->each(function($user) {
+            /// adiciona a sua role no db
+            userRole::factory()->create([
+                'id_user' => $user->id
+            ]);
+        });
     }
 }
