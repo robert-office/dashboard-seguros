4c5625cf9ce722e649b959feeca879dc0eb1011b
robert-office <robertuillians123@gmail.com>
2022-04-18 16:55:25 -0300

    backend

--- backend/config/database.php
+++ backend/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://niftztiqsugbsp:df05ca2fa1dda63fdada69916d7c4bc86d14fc5e451f13b3e4166af4c81c4343@ec2-54-173-77-184.compute-1.amazonaws.com:5432/deugfp7a3c9eag',
+            'host' => 'ec2-54-173-77-184.compute-1.amazonaws.com',
+            'port' => '5432',
+            'database' => 'deugfp7a3c9eag',
+            'username' => 'niftztiqsugbsp',
+            'password' => 'df05ca2fa1dda63fdada69916d7c4bc86d14fc5e451f13b3e4166af4c81c4343',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
