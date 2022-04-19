7db3988e2d412176b8b62002756e4d0014e8a454
robert-office <robertuillians123@gmail.com>
2022-04-19 15:31:12 -0300

    error cors

---
+++ backend/app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        //Acrescente as 3 linhas abaixo
+        $response->headers->set('Access-Control-Allow-Origin', "*");
+        $response->headers->set('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS");
+        $response->headers->set('Access-Control-Allow-Headers', "Content-Type, Accept, Authorization, X-Requested-With, Application");
+
+        return $response;
+    }
+}
