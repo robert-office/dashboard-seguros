687ecc2ec90319ba47b6b4b2e8726a5caf1d3029
robert-office <robertuillians123@gmail.com>
2022-04-19 15:18:48 -0300

    error cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -18,16 +18,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://acompany-dashboard.netlify.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
