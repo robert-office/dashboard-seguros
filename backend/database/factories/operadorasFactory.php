1806d040e8f7af2836678c7040e69052329dadc6
robert-office <robertuillians123@gmail.com>
2022-04-19 14:02:57 -0300

    nome errado da factory

--- backend/database/factories/operadorasFactory.php
+++ backend/database/factories/operadorasFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\operadoras>
  */
-class operadoraFactory extends Factory
+class operadorasFactory extends Factory
 {
     /**
      * Define the model's default state.
